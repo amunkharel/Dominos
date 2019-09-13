f57040ea1d0e2f2fa610492a32559a3392b4d27d
Amun Kharel <akharel@cs.unm.edu>
2019-09-13 10:37:06 -0600

    Start JavaDoc Documentation

--- src/dominos/Board.java
+++ src/dominos/Board.java
@@ -1,29 +1,46 @@
+/**
+ * Project 2 - CS351, Fall 2019, Class for board where dominos are placed
+ * @version Date 2019-09-13
+ * @author Amun Kharel
+ *
+ *
+ */
+
 package dominos;
-
 import java.util.ArrayList;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Board {
+    /** Checks if nothing is placed in the dominos*/
     private boolean isEmpty;
 
+    /** Checks how many dominos are placed in top left*/
     private int topLeftIndex;
 
+    /**Checks how many dominos are placed in bottom left */
     private int bottomLeftIndex;
 
+    /** Maps if left is at top/bottom and right is at top/bottom */
     private Map<Character, Character> dominoPosition;
 
+    /**List of all dominos placed in the board */
     private List<Domino> allDominos;
 
+    /**List of dominos placed in the top */
     private List<Domino> topDominos;
 
+    /**List of dominos placed at the bottom */
     private List<Domino> bottomDominos;
 
+    /** Total number of dominos placed in the board*/
     int totalDominos = 0;
 
-
+    /**
+     * Constructor to initialize Board for the game
+     *
+     */
     public Board() {
 
         allDominos =  new ArrayList<Domino>();
@@ -44,13 +61,27 @@ public class Board {
 
     }
 
+    /**
+     * Checks if the board is empty and returns true or false values
+     *
+     * @return Boolean, returns true if empty and vice versa
+     */
+
     public boolean isEmpty() {
         return isEmpty;
     }
 
+    /**
+     * Adds dominos in the board, top/ bottom depending on the algorithm
+     *
+     * @param int leftNode, leftNode of the domino to be added
+     * @param int rightNode, rightNode of the domino to be added
+     * @param String direction, direction where to add "l"/"r"
+     */
     public void addDominos(int leftNode, int rightNode, String direction) {
         Domino domino = new Domino(leftNode, rightNode);
 
+        //if the board is empty adds it at top center
         if(isEmpty) {
             allDominos.add(domino);
             topDominos.add(domino);
@@ -58,6 +89,10 @@ public class Board {
             dominoPosition.put('r', 't');
             isEmpty = false;
         }
+
+        //if direction is left and board is not empty,
+        //if current left position is bottom, adds to top
+        // and vice versa
         else {
             if(direction.equals("l")) {
                 if(dominoPosition.get('l') == 't'){
@@ -77,7 +112,7 @@ public class Board {
                     totalDominos++;
                 }
             }
-
+            //same logic for the right
             if(direction.equals("r")) {
                 if(dominoPosition.get('r') == 't') {
                     allDominos.add(domino);
@@ -97,6 +132,14 @@ public class Board {
 
     }
 
+    /**
+     * Returns true if move is valid, else returns false
+     *
+     * @param int leftNode, leftNode of the domino to be added
+     * @param int rightNode, rightNode of the domino to be added
+     * @param String direction, direction where to add "l"/"r"
+     * @return boolean , returns true if move is valid, else returns false
+     */
     public boolean isValidMove(int leftNode, int rightNode, String direction) {
         if(direction.equals("l")) {
             if(allDominos.get(0).getLeftNode() == rightNode ||
@@ -107,7 +150,8 @@ public class Board {
 
         if(direction.equals("r")) {
             if(allDominos.get(totalDominos).getRightNode() == leftNode
-                    || leftNode == 0 || allDominos.get(totalDominos).getRightNode() == 0) {
+                    || leftNode == 0 ||
+                    allDominos.get(totalDominos).getRightNode() == 0) {
                 return true;
             }
         }
@@ -115,18 +159,25 @@ public class Board {
     }
 
 
+    /**
+     * Prints Console board of the game
+     *
+     */
     public void printBoard() {
 
-        System.out.println("------------Game Board---------------------------------------------------------");
+        System.out.println("------------Game Board------------" +
+                "---------------------------------------------");
         if(bottomLeftIndex > topLeftIndex) {
             System.out.print("   ");
             for (int i = 0; i < topDominos.size(); i++) {
-                System.out.print("(" + topDominos.get(i).getLeftNode() + ", " + topDominos.get(i).getRightNode()+ ")");
+                System.out.print("(" + topDominos.get(i).getLeftNode() +
+                        ", " + topDominos.get(i).getRightNode()+ ")");
             }
             System.out.println(" ");
 
             for (int i = 0; i < bottomDominos.size(); i++) {
-                System.out.print("(" + bottomDominos.get(i).getLeftNode() + ", " + bottomDominos.get(i).getRightNode()+ ")");
+                System.out.print("(" + bottomDominos.get(i).getLeftNode() +
+                        ", " + bottomDominos.get(i).getRightNode()+ ")");
             }
             System.out.println(" ");
 
@@ -134,41 +185,70 @@ public class Board {
 
         else {
             for (int i = 0; i < topDominos.size(); i++) {
-                System.out.print("(" + topDominos.get(i).getLeftNode() + ", " + topDominos.get(i).getRightNode()+ ")");
+                System.out.print("(" + topDominos.get(i).getLeftNode() +
+                        ", " + topDominos.get(i).getRightNode()+ ")");
             }
             System.out.println(" ");
 
             System.out.print("   ");
             for (int i = 0; i < bottomDominos.size(); i++) {
-                System.out.print("(" + bottomDominos.get(i).getLeftNode() + ", " + bottomDominos.get(i).getRightNode()+ ")");
+                System.out.print("(" + bottomDominos.get(i).getLeftNode() +
+                        ", " + bottomDominos.get(i).getRightNode()+ ")");
             }
 
             System.out.println(" ");
         }
 
-        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("------------------------------" +
+                "-------------------------------------------------");
     }
+
+    /**
+     * Returns all dominos in the board
+     *
+     * @return List<Domino> , returns all dominos present in the board
+     */
 
     public List<Domino> getAllDominos() {
         return allDominos;
     }
 
+    /**
+     * Returns total dominos in the board
+     * @return int , returns total number of dominos in the board
+     */
     public int getTotalDominos() {
         return totalDominos;
     }
 
+    /**
+     * Returns index of bottom left
+     * @return int , returns index of bottom left
+     */
     public int getBottomLeftIndex() {
         return bottomLeftIndex;
     }
 
+    /**
+     * Returns index of top left
+     * @return int , returns index of top left
+     */
     public int getTopLeftIndex() {
         return topLeftIndex;
     }
 
+    /**
+     * Returns list of bottom Dominos
+     * @return int , returns list of bottom dominos
+     */
     public List<Domino> getBottomDominos() {
         return bottomDominos;
     }
 
+    /**
+     * Returns list of top dominos
+     * @return int , returns list of top dominos
+     */
     public List<Domino> getTopDominos() {
         return topDominos;
     }
--- src/dominos/Main.java
+++ src/dominos/Main.java
@@ -1,5 +1,12 @@
-package dominos;
+/**
+ * Project 2 - CS351, Fall 2019, Main Class to run the Dominos Game
+ * @version Date 2019-09-13
+ * @author Amun Kharel
+ *
+ *
+ */
 
+package dominos;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -14,31 +21,51 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    /** Boneyard of the game where we draw from: largest Number is 6*/
     private Boneyard boneyard = new Boneyard(6);
 
+    /** Board where we each player place their dominos*/
     private Board board = new Board();
 
+    /** Human Player of the game*/
     private  HumanPlayer p1 = new HumanPlayer(boneyard, board);
 
+    /** Computer Player of the game*/
     private ComputerPlayer p2 = new ComputerPlayer(boneyard, board);
 
+    /** Object that handles the click in the board*/
     private EventHandler e;
 
+    /** Text to show number of boneyard in the GUI*/
     private Text numberOfBoneyard = new Text();
 
+    /** Text to show number of computer dominos in the GUI*/
     private Text numberOfComputerDominos = new Text();
 
+    /** BorderPane layout for the game*/
     private BorderPane bp = new BorderPane();
 
+    /** Canvas to draw the game board and player's tray*/
     private Canvas canvas = new Canvas(1200, 600);
 
+    /** starting postion of y-axis of player's tray*/
     private int dominoStartY = 500;
 
+    /** Dynamic GUI object for the game*/
     private GameGui gui = new GameGui(p1, board,canvas, dominoStartY);
 
+    /** Scene where the pane is stored in the game*/
     private Scene scene = new Scene(bp, 1200, 1000);
 
+    /** Alert dialog box after game is over */
     private Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+    /**
+     * Method where the program starts. This method pulls out logic and gui board
+     * to make this game work
+     *
+     * @param Stage stage, Stage where the Board GUI for this game is placed
+     */
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -49,16 +76,25 @@ public class Main extends Application {
 
         System.exit(0); */
 
-        numberOfBoneyard.setText("Boneyard contains " + boneyard.getNumberOfDominosInBoneyard() + " dominos");
+        //Shows number of dominos left in boneyard
+        numberOfBoneyard.setText("Boneyard contains " +
+                boneyard.getNumberOfDominosInBoneyard() + " dominos");
         numberOfBoneyard.setFont(Font.font("Verdana", 20));
 
-        numberOfComputerDominos.setText("Computer has " + p2.getTray().getComputerDominos().size() + " dominos");
+        //text that shows number of dominos computer holds
+        numberOfComputerDominos.setText("Computer has " +
+                p2.getTray().getComputerDominos().size() + " dominos");
         numberOfComputerDominos.setFont(Font.font("Verdana", 20));
 
+
+        //mouse-click event handler for the game. If game is over
+        //shows who won in the dialog box
         canvas.setOnMousePressed(new javafx.event.EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                e = new EventHandler(p1, p2, board, event.getX(), event.getY(), boneyard, numberOfBoneyard, numberOfComputerDominos);
+                e = new EventHandler(p1, p2, board,
+                        event.getX(), event.getY(), boneyard,
+                        numberOfBoneyard, numberOfComputerDominos);
                 e.handleEvent();
 
                 if(e.isGameOver()) {
@@ -71,7 +107,8 @@ public class Main extends Application {
 
                     if(p1.countScore() < p2.countScore()) {
                         alert.setTitle("Game Over");
-                        alert.setContentText("Congratulations!! You have won the game");
+                        alert.setContentText("Congratulations!!" +
+                                " You have won the game");
                         alert.showAndWait();
                         primaryStage.close();
                     }
@@ -87,6 +124,7 @@ public class Main extends Application {
             }
         });
 
+        //animation timer where GUI is refreshed periodically
         AnimationTimer animator = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -98,6 +136,7 @@ public class Main extends Application {
 
         animator.start();
 
+        //placement of canvas, and other texts
         bp.setCenter(canvas);
 
         bp.setTop(numberOfBoneyard);
